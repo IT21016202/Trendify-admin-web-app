335930d6d4e2d4d44eda9b4af405dabd3dcc5a55
IT21016202 <it21016202@my.sliit.lk>
2024-10-05 14:30:01 +0530

    all orders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate, RouterProvider } from "react-router-dom";
+
+import AllOrders from './components/AllOrders';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/orders" element={<AllOrders />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/AllOrders.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'; 
+
+const AllOrders = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+      axios.get('https://localhost:7022/api/order')
+      .then((response) => {
+        //console.log(response.data);
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>All Orders</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Customer ID</th>
+            <th>Products</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Order Date</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order, index) => {
+            return (
+              <tr key={order.id}>
+                <td>{index+1}</td>
+                <td>{order.userId}</td>
+                <td>N/A</td>
+                <td>N/A</td>
+                <td>{order.orderTotal}</td>
+                <td>{new Date(order.orderDate).toLocaleDateString()}</td>
+                <td>{order.status}</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* <Link to="/add-order">Add Order</Link> */}
+    </div>
+  )
+}
+
+export default AllOrders
